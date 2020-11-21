61e6c3543dad56de81605a51baa4b7bc7b4f688e
Serbur <64693577+Serbur@users.noreply.github.com>
2020-11-21 21:55:37 +0400

    lol

--- JavaPr3/src/com/company/Main.java
+++ JavaPr3/src/com/company/Main.java
@@ -6,7 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter number: ");
-        int enter = in.nextInt();
-    }
-}
+        System.out.print("Enter amount of numbers: ");
+        int num = in.nextInt();
+        System.out.print("Enter range a: ");
+        int a = in.nextInt();
+        System.out.print("Enter range b: ");
+        int b = in.nextInt();
+        for (int i = 0; i < num; i++) {
+            int nums = a + (int) (Math.random() * ((b - a) + 1));
+            System.out.println("\n Number: " + nums);
+        }
+}}
