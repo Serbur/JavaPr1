dad699e22382e6443ac0832371bb685b62fb7987
Serbur <64693577+Serbur@users.noreply.github.com>
2021-03-18 16:15:52 +0400

    nice

---
+++ JavaDD/src/example/Main.java
@@ -0,0 +1,33 @@
+package example;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter: ");
+        String str = sc.nextLine();
+        Pattern pattern = Pattern.compile("[a-z0-9)(]+");
+        Matcher matcher = pattern.matcher(str);
+        boolean found = matcher.matches();
+        if(found)
+        {
+            String[] subStr = str.split("[\\W]");
+        for (int i=0 ; i<subStr.length ; i++) {
+            if (subStr[i].matches("[0-9]+")) {
+                int num = Integer.parseInt(subStr[i]);
+                int counter = 0;
+                while (counter < num){
+                    System.out.print(subStr[i+1]);
+                    counter ++;
+                }
+            }
+        }}
+        else{
+            System.out.println("Incorrect input");
+        }
+    }}
+
