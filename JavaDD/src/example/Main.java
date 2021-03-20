cba4d020b91349491b46eb30cfa4a072d45e77d9
Serbur <64693577+Serbur@users.noreply.github.com>
2021-03-20 12:53:11 +0400

    testing

--- JavaDD/src/example/Main.java
+++ JavaDD/src/example/Main.java
@@ -10,24 +10,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter: ");
         String str = sc.nextLine();
-        Pattern pattern = Pattern.compile("[a-z0-9)(]+");
+        Pattern pattern = Pattern.compile("[a-z0-9\\[|\\]$]+");
         Matcher matcher = pattern.matcher(str);
         boolean found = matcher.matches();
         if(found)
         {
             String[] subStr = str.split("[\\W]");
-        for (int i=0 ; i<subStr.length ; i++) {
-            if (subStr[i].matches("[0-9]+")) {
-                int num = Integer.parseInt(subStr[i]);
-                int counter = 0;
-                while (counter < num){
-                    System.out.print(subStr[i+1]);
-                    counter ++;
-                }
+            for (int i=0 ; i<subStr.length ; i++) {
+                if (subStr[i].matches("[0-9]+")) {
+                    int num = Integer.parseInt(subStr[i]);
+                    int counter = 0;
+                    while (counter < num){
+                        System.out.print(subStr[i+1]);
+                        counter ++;
+                    }}
             }
-        }}
+        }
         else{
             System.out.println("Incorrect input");
         }
-    }}
-
+    }
+}
